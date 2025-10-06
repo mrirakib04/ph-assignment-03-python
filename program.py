ca734cb8416481f563ac5c375c973f1bebaa1e36
mrirakib04 <mdrakibulislam11928@gmail.com>
2025-10-06 21:08:19 +0600

    py converted Problem -02 ( Cycle or Laptop ) added

--- program.py
+++ program.py
@@ -4,3 +4,15 @@ area = 800
 if area > 0 and area <= 10**9:
     res1 = area / 2
     print(res1)
+
+
+# Problem -02 ( Cycle or Laptop )
+money = 10000
+# write your code here
+if money > 0 and money <= 10**9:
+    if money >= 25000:
+        print("Laptop")
+    elif money >= 10000 and money < 25000:
+        print("Cycle")
+    else:
+        print("Chocolate")
